6e4a79d6eaef9fad0b621761b7f7a87b553d2329
rbaluta17dw <rbaluta17dw@ikzubirimanteo.com>
2018-05-29 13:54:04 +0200

    Imagen por link

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,28 +11,34 @@ export class HomePage {
 	personajeE:any;
 	nombre:string;
 	descripcion:any;
+	imagen:any;
 
 	constructor(public navParams: NavParams, public modal: ModalController) {
 		this.personajes=[
 		{
 			nombre:"Loki",
-			descripcion:"Es un pavo que muere al principio"
+			descripcion:"Es un pavo que muere al principio",
+			imagen:"https://vignette.wikia.nocookie.net/shipping/images/b/b7/Avengers_-_Loki_%28uniqueLegend%29.jpg/revision/latest?cb=20130910075950"
 		},
 		{
 			nombre:"Gamora",
-			descripcion:"Es un pavo que muere al principio"
+			descripcion:"Es un pavo que muere al principio",
+			imagen:"http://versatileleather.com/wp-content/uploads/2014/11/guardians-of-the-galaxy-gamora-costume-2.jpg"
 		},		
 		{
 			nombre:"Iron-Man",
-			descripcion:"Es un pavo que muere al principio"
+			descripcion:"Es un pavo que muere al principio",
+			imagen:"https://wallpapersite.com/images/wallpapers/iron-man-3840x2160-avengers-infinity-war-4k-12794.jpg"
 		},		
 		{
 			nombre:"Thor",
-			descripcion:"Es un pavo que muere al principio"
+			descripcion:"Es un pavo que muere al principio",
+			imagen:"https://www.sideshowtoy.com/wp-content/uploads/2018/03/marvel-avengers-infinity-war-thor-sixth-scale-figure-hot-toys-feature-903422.jpg"
 		},		
 		{
 			nombre:"Vision",
-			descripcion:"Es un pavo que muere al principio"
+			descripcion:"Es un pavo que muere al principio",
+			imagen:"http://www.comingsoon.net/assets/uploads/2017/03/Avengers-Age-of-Ultron_poster_goldposter_com_151-1.jpg"
 		}
 	];
 }
