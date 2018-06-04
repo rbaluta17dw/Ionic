bf456888a1809295e63c7e3f14e5cd0a37eeb358
rbaluta17dw <rbaluta17dw@ikzubirimanteo.com>
2018-06-04 12:11:49 +0200

    cambios inutiles

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { homePage } from '../home/home';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+	selector: 'page-about',
+	templateUrl: 'about.html'
 })
 export class AboutPage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+	personaje:any[];
+	nombre:string;
+	descripcion:any;
+	imagen:any;
+	constructor(public navCtrl: NavController) {
+		this.personaje=[
+		{
+			nombre:" ",
+			descripcion:" ",
+			imagen:" "
+		}];
+	}
+	insert(){
+		this.navCtrl.push(homePage, {
+			personaje:this.personaje
+		});
+	}
 }
